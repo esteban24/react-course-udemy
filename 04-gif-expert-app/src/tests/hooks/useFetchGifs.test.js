49e8b7508d80d9e07eaa5422c9caed99c21f83be
Esteban Martín <esteban.martin.busto@gmail.com>
2021-07-01 10:30:34 +0200

    Completed section 04

--- 04-gif-expert-app/src/tests/hooks/useFetchGifs.test.js
+++ 04-gif-expert-app/src/tests/hooks/useFetchGifs.test.js
@@ -1,8 +1,25 @@
-import { useFetchGifs } from "../../hooks/useFetchGifs"
+import { useFetchGifs } from "../../hooks/useFetchGifs";
+import { renderHook } from '@testing-library/react-hooks';
 
 describe('useFetchGifs.js test suite', () => {
-    test('should return initial state', () => {
-        useFetchGifs
-    })
-    
-})
+    test('should return initial state', async() => {
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs('One Punch') );
+        const { data, loading } = result.current;
+
+        await waitForNextUpdate();
+
+
+    });
+
+    test('should return arrray of images and loading false', async() => {
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs('One Punch') );
+
+        await waitForNextUpdate();
+
+        const { data, loading } = result.current;
+
+        expect(data.length).toBe(10);
+        expect(loading).toBe(false);
+    });
+
+});
