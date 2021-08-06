754c079c3358a986589ac9feab48fb6d0f1670e0
Esteban <emartin@datiobd.com>
2021-08-06 21:26:07 +0200

    Journal app

---
+++ 08-journal-app/src/components/journal/Sidebar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { JournalEntries } from './JournalEntries'
+
+export const Sidebar = () => {
+    return (
+        <aside className="journal__sidebar">
+            <div className="journal__sidebar-navbar">
+                <h3 className="mt-5">
+                    <i className="far fa-moon" />
+                    <span> Journal Menu</span>
+                </h3>
+                <button className="btn">
+                    Logout
+                </button>
+            </div>
+            <div className="jorunal__new-entry">
+                <i className="far fa-calendar-plus fa-5x"/>
+                <p className="mt-5">
+                    New entry
+                </p>
+            </div>
+
+            <JournalEntries />
+        </aside>
+    )
+}
---
+++ 08-journal-app/src/routers/AppRouter.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Redirect,
+    Route,
+    Switch
+} from "react-router-dom";
+
+import { JournalScreen } from '../components/journal/JournalScreen';
+import { AuthRouter } from './AuthRouter';
+
+export const AppRouter = () => {
+    return (
+        <Router>
+            <div>
+                <Switch>
+                    <Route
+                        path="/auth"
+                        component={ AuthRouter }
+                    />
+                    <Route
+                        path="/"
+                        exact
+                        component={ JournalScreen }
+                    />
+                    <Redirect to="/auth/login"/>
+                </Switch>
+            </div>
+        </Router>
+    )
+}
